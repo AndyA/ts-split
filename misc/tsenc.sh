5172ddcfb2aea4ce5951136334e37b5721bf6d27
Andy Armstrong <andy@hexten.net>
2011-03-23 01:24:57 +0000

    Get the - in the right place.

--- misc/tsenc.sh
+++ misc/tsenc.sh
@@ -6,7 +6,7 @@ AUDIO_NONE="-an"
 AUDIO_OPTIONS="-acodec libfaac -ac 2 -ab 96k"
 VIDEO_OPTIONS="-g 100 -keyint_min 50 -flags2 -mbtree -threads 0"
 VIDEO_PASS1="-vcodec libx264 -vpre veryfast_firstpass ${VIDEO_OPTIONS}"
-VIDEO_PASS2=-"vcodec libx264 -vpre veryfast ${VIDEO_OPTIONS}"
+VIDEO_PASS2="-vcodec libx264 -vpre veryfast ${VIDEO_OPTIONS}"
 BITRATE="800k"
 SIZE="768x320"
 
